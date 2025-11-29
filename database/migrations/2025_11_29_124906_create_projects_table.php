15d7e4a6d2244d24ae5aafecfea59d7600ddb8ce
AnikKumarKuri <kuri22205101077@diu.edu.bd>
2025-11-29 18:52:57 +0600

    project table migration

--- database/migrations/2025_11_29_124906_create_projects_table.php
+++ database/migrations/2025_11_29_124906_create_projects_table.php
@@ -6,20 +6,20 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('image')->nullable();      // thumbnail
+            $table->string('live_link')->nullable();
+            $table->string('github_link')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('projects');
