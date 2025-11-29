693f6e9a60ee90f2a2c2860c411642ce7c3b21e5
AnikKumarKuri <kuri22205101077@diu.edu.bd>
2025-11-29 19:00:53 +0600

    setting route

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::prefix('admin')->group(function () {
+    Route::resource('projects', ProjectController::class);
+});
