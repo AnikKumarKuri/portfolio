94c61858c1685a0b45162621db336e74221daeb7
AnikKumarKuri <kuri22205101077@diu.edu.bd>
2025-12-05 19:32:56 +0600

    model-profile

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    //
+    protected $fillable = [
+        'name','title','subtitle','about','profile_image','cv_link',
+        'email','phone','location',
+        'github','linkedin','facebook','twitter'
+    ];
 }
